e19d31291597d4c3637c1025745a0c4b6a78b75b
joe <joe@egmrc.com>
2026-06-24 14:42:41 -0600

    Update passagesHero

--- src/blocks/PassagesHero/Component.tsx
+++ src/blocks/PassagesHero/Component.tsx
@@ -67,11 +67,11 @@ export const PassagesHeroBlock = async ({
 
       <div className="relative z-50 flex flex-col justify-end 2xl:absolute 2xl:inset-0 2xl:min-h-0">
         <div className="flex min-h-0 flex-1 flex-col justify-between py-6 md:py-8">
-          <div className="grid grid-cols-4 gap-x-16 gap-y-0 2xl:grid-cols-12 md:-translate-y-20">
+          <div className="grid grid-cols-4 gap-x-16 gap-y-0 2xl:grid-cols-12 ">
             {logo && typeof logo === 'object' ? (
               <div className="col-span-4 xl:col-span-5 xl:col-start-4">
                 <Media
-                  className="relative h-45 w-full max-w-84 md:h-60 xl:h-80"
+                  className="relative h-45 w-full max-w-84 md:h-60 xl:h-80 md:-translate-y-20"
                   fill
                   imgClassName="object-contain object-left-top"
                   pictureClassName="block h-full w-full"
